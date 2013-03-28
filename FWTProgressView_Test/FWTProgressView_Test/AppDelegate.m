9795a569ebf027695a1c290d1e83a4bd620b5676
Marco Meschini <marco@futureworkshops.com>
2013-03-28 17:22:05 +0000

    Make it nicer!

--- FWTProgressView_Test/FWTProgressView_Test/AppDelegate.m
+++ FWTProgressView_Test/FWTProgressView_Test/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "SamplePickerViewController.h"
 
 @implementation AppDelegate
 
@@ -20,13 +19,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
-//    self.window.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
-//    
-//    UIViewController *vc = [[[SamplePickerViewController alloc] init] autorelease];
-//    UINavigationController *nc = [[[UINavigationController alloc] initWithRootViewController:vc] autorelease];
-//    self.window.rootViewController = nc;
-//    [self.window makeKeyAndVisible];
-    
     self.window.backgroundColor = [UIColor whiteColor];
     
     //
@@ -35,28 +27,11 @@
                          [RistrettoSampleDescriptor descriptorWithTitle:@"Custom tracks" className:@"TableViewController"],
                          ];
     rootViewController.items = samples;
-    rootViewController.title = @"FWTProgressView";
-    
+
     //
-    UIView *headerView = [[[UIView alloc] initWithFrame:(CGRect){.0f, .0f, 240.0f, 200.0f}] autorelease];
-    [rootViewController setTableHeaderView:headerView insets:(UIEdgeInsets){58.0f, 40.0f, 45.0f, 40.0f}];
-    
-    UIImageView *iconView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon_progressview.png"]] autorelease];
-    iconView.center = CGPointMake(CGRectGetMidX(headerView.bounds), iconView.center.y);
-    [headerView addSubview:iconView];
-    
-    UILabel *label = [[[UILabel alloc] init] autorelease];
-    label.text = @"Choose a sample:";
-    label.font = [UIFont Ristretto_lightFontOfSize:18.0f];
-    label.textColor = [UIColor Ristretto_darkGrayColor];
-    [label sizeToFit];
-    label.center = CGPointMake(CGRectGetMidX(headerView.bounds), CGRectGetHeight(headerView.bounds)-CGRectGetHeight(label.bounds)*.5f);
-    [headerView addSubview:label];
-    
-    self.window.rootViewController = [UINavigationController Ristretto_navigationControllerWithRootViewController:rootViewController];
-    
+    self.window.rootViewController = [UINavigationController Ristretto_navigationControllerWithRootViewController:rootViewController
+                                                                                             defaultHeaderEnabled:YES];
     [self.window makeKeyAndVisible];
-    
     return YES;
 }
 
